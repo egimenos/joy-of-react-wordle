9740e42fd2a3c41fa9b2df44309944c79fe99c86
EgimenoS <ergisan@gmail.com>
2023-03-18 21:18:53 +0100

    wordle: exercise 3 v2

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -2,13 +2,6 @@ import React from "react";
 import Guess from "../Guess/Guess";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-const Guesses = ({ guesses }) => {
-  if (guesses.length === 0) {
-    return null;
-  }
-
-  return guesses.map((guess) => <Guess key={Math.random()} guess={guess} />);
-};
 const emptyGuess = " ".repeat(5);
 
 function PreviousGuesses({ guesses }) {
@@ -18,7 +11,9 @@ function PreviousGuesses({ guesses }) {
 
   return (
     <div className="guess-results">
-      {<Guesses guesses={[...guesses, ...emptyGuesses]} />}
+      {[...guesses, ...emptyGuesses].map((guess) => (
+        <Guess key={Math.random()} guess={guess} />
+      ))}
     </div>
   );
 }
